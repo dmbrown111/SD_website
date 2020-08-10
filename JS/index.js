f437fe37ab3e52a715b6ee06f8dbfcbfb999e00d
dmbrown111 <dmbrown111@gmail.com>
2020-08-10 12:18:05 -0500

    cleaned up files

--- JS/index.js
+++ JS/index.js
@@ -8,14 +8,3 @@ function toggleMenu() {
 }
 
 document.getElementById("burgerButton").addEventListener("click", toggleMenu);
-
-//JavaScript Animation Version//
-
-// function openMenu(){
-//   document.getElementById("menuContentContainer").animate([
-//     {transform: 'translateY(-305px)'},
-//     {transform: 'translateY(0px)'}
-//   ],{duration:1000, iteration:Infinity})
-
-// }
-// document.getElementById("burgerButton").addEventListener("click", openMenu);
